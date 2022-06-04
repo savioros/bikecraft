8f576d485e3ad82ef083a0eb87510e640347fff1
Sávio Rian <saviopeck@outlook.com>
2022-06-04 00:11:17 -0300

    Criação do script que seleciona o item a ser comprado por parâmetro e script que expande as perguntas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Links ativados */
 const links = document.querySelectorAll('.header-menu a')
 
 function ativarLink(link){
@@ -10,3 +11,36 @@ function ativarLink(link){
 }
 
 links.forEach(ativarLink)
+
+/* Ativar itens do orçamento */
+const parametros = new URLSearchParams(location.search)
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro)
+
+    if(elemento){
+        elemento.checked = true
+    }
+
+}
+
+parametros.forEach(ativarProduto)
+
+/* Expandir Perguntas */
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget
+    const controls = pergunta.getAttribute('aria-control')
+    const resposta = document.getElementById(controls)
+
+    resposta.classList.toggle('ativa')
+    const ativa = resposta.classList.contains('ativa')
+    pergunta.setAttribute('aria-expanded', ativa)
+}
+
+function eventosPergunta(pergunta){
+    pergunta.addEventListener("click", ativarPergunta)
+}
+
+perguntas.forEach(eventosPergunta)
